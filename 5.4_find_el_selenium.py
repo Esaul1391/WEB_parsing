3f5da4face12a6bc1d5d14d8f1625336fd8b3e99
Esaul1391 <1391esaul@gmail.ru>
2023-06-26 07:37:34 +0300

    task4 done

--- 5.4_find_el_selenium.py
+++ 5.4_find_el_selenium.py
@@ -115,12 +115,97 @@ import time
 # with webdriver.Chrome() as browser:
 #     browser.get(link)
 #     link = browser.find_element(By.LINK_TEXT, "16243162441624")
-import time
+
+#           task6
+
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+#
+# with webdriver.Chrome() as browser:
+#     browser.get('https://parsinger.ru/selenium/3/3.html')
+#     button = browser.find_elements(By.TAG_NAME, "p")
+#     time.sleep(2)
+#     print(sum([int(i.text) for i in button]))
+
+#           task7
+
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+#
+# with webdriver.Chrome() as browser:
+#     browser.get('https://parsinger.ru/selenium/3/3.html')
+#     button = browser.find_elements(By.CLASS_NAME, "text")
+#     #links = browser.find_elements(By.CSS_SELECTOR, '.text p:nth-child(2)')
+#     time.sleep(2)
+#     print(sum([int(i.text.split('\n')[1]) for i in button]))
+
+
+
+#           task7
+
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+#
+# with webdriver.Chrome() as browser:
+#     browser.get('https://parsinger.ru/selenium/4/4.html')
+#     button = browser.find_elements(By.CLASS_NAME, "check")
+#     #links = browser.find_elements(By.CSS_SELECTOR, '.text p:nth-child(2)')
+#     print(len(button))
+#     for i in range(len(button)):
+#         button[i].click()
+#     res = browser.find_element(By.CLASS_NAME, "btn").click()
+#     res1 = browser.find_element(By.ID, "result")
+#     time.sleep(20)
+#     print(res1.text)
+
+
+#           task8
+
+# Когда вам необходимо получить значение атрибута. Вы можете использовать метод
+# .get_attribute('attribute'), Где attribute - это имя требуемого вам атрибута.
+#
+# Если необходимо получить значение value="" мы напишем следующий код.
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+#
+# with webdriver.Chrome() as browser:
+#     browser.get('http://parsinger.ru/selenium/5/5.html')
+#     checkbox = browser.find_elements(By.CLASS_NAME, 'check')
+#     for item in checkbox:
+#         print(item.get_attribute('value'))
+
+
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+numbers = [1, 2, 3, 4, 8, 9, 11, 12, 13, 14, 15, 16, 17, 22, 23, 28, 29, 33, 34, 38,
+39, 43, 44, 48, 49, 51, 52, 53, 54, 55, 56, 57, 58, 61, 62, 63, 64, 68, 69, 73,
+74, 78, 79, 83, 84, 88, 89, 91, 92, 97, 98, 101, 104, 108, 109, 113, 114, 118,
+119, 123, 124, 128, 129, 131, 132, 137, 138, 140, 141, 144, 145, 148, 149, 153,
+154, 158, 159, 163, 164, 165, 168, 169, 171, 172, 177, 178, 180, 181, 184, 185,
+187, 188, 189, 190, 192, 193, 194, 195, 197, 198, 199, 200, 204, 205, 206, 207,
+208, 209, 211, 212, 217, 218, 220, 221, 224, 225, 227, 228, 229, 230, 232, 233,
+234, 235, 237, 238, 239, 240, 245, 246, 247, 248, 249, 251, 252, 253, 254, 255,
+256, 257, 258, 260, 261, 264, 265, 268, 269, 273, 274, 278, 279, 288, 289, 291,
+292, 293, 294, 295, 296, 297, 300, 301, 302, 303, 304, 305, 308, 309, 313, 314,
+318, 319, 328, 329, 331, 332, 339, 340, 341, 342, 343, 344, 345, 346, 348, 349,
+353, 354, 358, 359, 368, 369, 371, 372, 379, 380, 385, 386, 408, 409, 411, 412,
+419, 420, 425, 426, 428, 429, 433, 434, 438, 439, 444, 445, 446, 447, 448, 451,
+452, 459, 460, 465, 466, 467, 468, 469, 470, 472, 473, 474, 475, 477, 478, 479,
+480, 485, 486, 487, 488, 491, 492, 499, 500, 505, 506, 508, 509, 513, 514, 518, 519]
 
 with webdriver.Chrome() as browser:
-    browser.get('https://parsinger.ru/selenium/2/2.html')
-    button = browser.find_element(By.LINK_TEXT, "16243162441624").click()
-    time.sleep(10)
-
+    browser.get('https://parsinger.ru/selenium/5/5.html')
+    checkbox = browser.find_elements(By.CLASS_NAME, 'check')
+    for i in range(len(checkbox)):
+        if int(checkbox[i].get_attribute('value')) in numbers:
+            #print(checkbox[i])
+            checkbox[i].click()
+    res = browser.find_element(By.CLASS_NAME, "btn").click()
+    res1 = browser.find_element(By.ID, "result")
+    time.sleep(20)
+    print(res1.text)
