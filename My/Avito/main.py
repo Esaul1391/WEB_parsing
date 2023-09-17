197c38a99924dde01bc6b221bf761ba83283441c
Esaul1391 <1391esaul@gmail.com>
2023-09-17 15:34:34 +0300

    Asus

--- My/Avito/main.py
+++ My/Avito/main.py
@@ -1,7 +1,5 @@
 import undetected_chromedriver as uc
-
-driver = uc.Chrome(version_main=116)  # wride my version
-driver.get('https://avito.ru')
+from selenium.webdriver.common.by import By
 
 
 class AvitoParse:
@@ -12,13 +10,37 @@ class AvitoParse:
         self.version = version_main
 
     def __set_up(self):
-        ...
+        self.driver = uc.Chrome(version_main=self.version)  # write my version
 
-    def __paginstor(self):
-        pass
+    def __get_url(self):
+        self.driver.get(self.url)
+
+    def __paginator(self):
+        while self.driver.find_elements(By.CSS_SELECTOR, "[data-marker='pagination-button/next]"):  # don't add count
+            self.__parse_page()
+            self.driver.find_element(By.CSS_SELECTOR, "[data-marker='pagination-button/next]").click()
 
     def __parse_page(self):
-        pass
+        titles = self.driver.find_elements(By.CSS_SELECTOR, "[data-marker='item']")
+        for title in titles:
+            name = title.find_element(By.CSS_SELECTOR, "[itemprop='name']").text
+            description = title.find_element(By.CSS_SELECTOR, "[class*='styles-module-root']").text  #  * ставится когда требуется примерно сказать название элемента
+            link = title.find_element(By.CSS_SELECTOR, "[data-marker='item-title']").get_attribute('href')
+            price = title.find_element(By.CSS_SELECTOR, '[itemprop="price"]').get_attribute('content')
+            print(name, description, link, price)
+
 
     def parse(self):
-        pass
+        self.__set_up()
+        self.__get_url()
+        self.__paginator()
+        self.__parse_page()
+
+
+if __name__ == '__main__':
+    AvitoParse(url='https://www.avito.ru/moskva/predlozheniya_uslug/oborudovanie_proizvodstvo/proizvodstvo_obrabotka-ASgBAgICAkSYC7SfAaALiKAB?cd=1&p=1&q=3d+печать',
+               count=2,
+               version_main=116,
+               items=['3d печать']
+               ).parse()
+
